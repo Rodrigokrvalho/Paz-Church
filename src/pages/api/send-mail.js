354530bebfdea761a44def2cbf29b0886cef88ad
Rodrigo Carvalho <54452396+Rodrigokrvalho@users.noreply.github.com>
2021-07-28 15:41:54 -0300

    form cadastro enviando email

--- src/pages/api/send-mail.js
+++ src/pages/api/send-mail.js
@@ -1,6 +1,5 @@
-import nodemailer from 'nodemailer'
 export default function SendEmail(req, res) {
-
+  let nodemailer = require('nodemailer')
   const user = process.env.USERMAIL
   const pass = process.env.PASSMAIL
   const host = process.env.HOST
@@ -18,9 +17,13 @@ export default function SendEmail(req, res) {
 
   const mailData = {
     from: user,
-    to: 'rodrigos-carvalho@hotmail.com',
-    subject: 'Ola, ....',
-    text: "Email enviado pelo NEXT",
+    to: req.body.email,
+    subject: `Bem Vindo ${req.body.fullName}`,
+    text: `Seja muito bem vindo. Seu cadastro foi realizado com sucesso.`,
+    html: ` <div>
+              <h1>Olá, ${req.body.fullName}!</h1></br>
+              <h3>Seja muito bem vindo! Seu cadastro foi realizado com sucesso.</h3>
+            </div>`
   }
 
   transporter.sendMail(mailData)
